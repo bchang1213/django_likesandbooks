7c95eb8c7bf2e2a99db814cfc53c6867b955a854
Brian Chang <kyoukamu@gmail.com>
2017-09-25 20:50:24 -0700

    ORM practice with django

---
+++ assignment.py
@@ -0,0 +1,31 @@
+# Create the appropriate models and the appropriate relationship.  Design your models so that the following command would be supported
+
+# Book.objects.first().uploader - this should return the user who uploaded the book
+
+# User.objects.first().uploaded_books - this should return all the books that are uploaded by the first user
+
+# Book.objects.first().liked_users - this should return all the users who liked the first book
+
+# User.objects.first().liked_books - this should return all the books that were liked by the first user
+
+# Create 3 different user accounts
+
+# Have the first user create 2 books.
+
+# Have the second user create 2 other books.
+
+# Have the third user create 2 other books.
+
+# Have the first user like the last book and the first book
+
+# Have the second user like the first book and the third book
+
+# Have the third user like all books
+
+# Display all users who like the first book
+
+# Display the user who uploaded the first book
+
+# Display all users who like the second book
+
+# Display the user who uploaded the second book
